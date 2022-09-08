8be16218bebf4fef82ad6a66d13d40b8064f7ebf
Tuibaev Jalyn <sharrder.blendi.1@gmail.com>
2022-09-08 16:32:13 +0600

    Сверстал фронтенд

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import './App.css';
 
 const App = () => {
+    const getShorten = (e) => {
+        e.preventDefault();
+        console.log(e);
+    };
+
+
     return (
-        <div>
-          
-        </div>
+        <form onSubmit={getShorten} className='form'>
+            <h1 className='form__title'>Shorten your link</h1>
+            <input className='form__input' placeholder='Enter URL here' type="url" required/>
+            <button type='submit' className='form__btn'>Shorten!</button>
+        </form>
     );
 };
 
